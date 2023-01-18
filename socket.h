81e555e1180591a5ad3996c49953c9dc3c44fe0f
uped <git@uped.dev>
2023-01-18 17:21:16 +0100

    Some fixes

--- socket.h
+++ socket.h
@@ -12,9 +12,11 @@
 #include <unistd.h>
 #include <string_view>
 #include <stdexcept>
+#include <utility>
 #include <vector>
 #include <ranges>
 #include <utility>
+#include <variant>
 
 // #TODO is it correct?
 #include "cerrno"
@@ -22,8 +24,7 @@
 int errno;
 
 // #TODO windows support
-// #TODO implement rest of the api
-// #TODO auto conversion to network ints and vice versa
+// #TODO support for msghdr?
 
 // type aliasing functions to avoid conflicts with member functions
 static constexpr auto platform_send = send;
@@ -37,6 +38,12 @@ static constexpr auto platform_listen = listen;
 static constexpr auto platform_accept = accept;
 static constexpr auto platform_setsockopt = setsockopt;
 static constexpr auto platform_getsockopt = getsockopt;
+static constexpr auto platform_getnameinfo = getnameinfo;
+static constexpr auto platform_getpeername = getpeername;
+static constexpr auto platform_ntop = inet_ntop;
+static constexpr auto platform_pton = inet_pton;
+static constexpr auto platform_recvfrom = recvfrom;
+static constexpr auto platform_sendto = sendto;
 
 /**
  * Wrapper for system sockets with utility functions.
@@ -44,15 +51,32 @@ static constexpr auto platform_getsockopt = getsockopt;
  */
 class Socket {
 public:
-
-    // maybe separate to owning and not owning address?
     class Address {
     public:
-        explicit Address(sockaddr *sockAddr) : mSockAddr(sockAddr), owner(false) {};
+        explicit Address(sockaddr *sockAddr) : storage(reinterpret_cast<sockaddr_storage *>(sockAddr)),
+                                               mIsOwning(false) {};
+
+        Address(int family, std::string_view address) : Address(family) {
+            int status = 0;
+            std::variant<sockaddr_in *, sockaddr_in6 *> inetAddress = getAsInternet();
+            // we have to support both ipv4 and ipv6 variants
+            if (sockaddr_in **ipv4 = std::get_if<sockaddr_in *>(&inetAddress)) {
+                status = platform_pton(family, address.data(), &(*ipv4)->sin_addr);
+            } else if (sockaddr_in6 **ipv6 = std::get_if<sockaddr_in6 *>(&inetAddress)) {
+                status = platform_pton(family, address.data(), &(*ipv6)->sin6_addr);
+            }
+            if (status == -1) {
+                throw Socket::Error(strerror(errno));
+            }
+        }
+
+        explicit Address(int family) : Address() {
+            storage->ss_family = family;
+        }
 
         Address() {
-            owner = true;
-            mSockAddr = new sockaddr();
+            mIsOwning = true;
+            storage = new sockaddr_storage();
         }
 
         // disable copying
@@ -61,43 +85,61 @@ public:
         Address &operator=(const Address &) = delete;
 
         // move constructor
-        Address(Address &&address) noexcept: mSockAddr(address.mSockAddr), owner(address.owner) {
-            address.owner = false;
-        }
+        Address(Address &&address) noexcept: storage(std::exchange(address.storage, nullptr)),
+                                             mIsOwning(std::exchange(address.mIsOwning, false)) {}
 
         ~Address() {
-            if (owner) {
-                delete mSockAddr;
+            if (isOwning() && storage) {
+                delete storage;
             }
         };
 
         std::string toString() {
-            // #TODO refactor
-            // only callable for inet addresses
-            // change size depending on family?
-            if (mSockAddr->sa_family == AF_INET6) {
-                char result[INET6_ADDRSTRLEN];
-                inet_ntop(mSockAddr->sa_family, mSockAddr, result, INET6_ADDRSTRLEN);
-                return {result};
-            } else if (mSockAddr->sa_family == AF_INET) {
-                char result[INET_ADDRSTRLEN];
-                inet_ntop(mSockAddr->sa_family, mSockAddr, result, INET_ADDRSTRLEN);
-                return {result};
-            } else {
-                // throw
+            char result[INET6_ADDRSTRLEN];
+
+            std::variant<sockaddr_in *, sockaddr_in6 *> inetAddress = getAsInternet();
+            if (sockaddr_in **ipv4 = std::get_if<sockaddr_in *>(&inetAddress)) {
+                inet_ntop(AF_INET, &(*ipv4)->sin_addr, result, INET6_ADDRSTRLEN);
+            } else if (sockaddr_in6 **ipv6 = std::get_if<sockaddr_in6 *>(&inetAddress)) {
+                inet_ntop(AF_INET6, &(*ipv6)->sin6_addr, result, INET6_ADDRSTRLEN);
             }
+
+            return {result};
         }
 
-        [[nodiscard]] sockaddr *getPlatform() const {
-            return mSockAddr;
+        [[nodiscard]] sockaddr_storage* getSockStorage() const {
+            return storage;
+        }
+
+        [[nodiscard]] sockaddr *getSockAddr() const {
+            return reinterpret_cast<sockaddr *>(storage);
+        }
+
+        std::variant<sockaddr_in *, sockaddr_in6 *> getAsInternet() {
+            if (storage->ss_family == AF_INET) {
+                return reinterpret_cast<sockaddr_in *>(storage);
+            } else if (storage->ss_family == AF_INET6) {
+                return reinterpret_cast<sockaddr_in6 *>(storage);
+            }
+            throw Socket::Error(
+                    "Can't get an address as a internet address pointer for family other than AF_INET or AF_INET6");
+        }
+
+        [[nodiscard]] bool isOwning() const {
+            return mIsOwning;
         }
 
     private:
-        sockaddr *mSockAddr;
-        // is this object responsible for deleting sock addr?
-        bool owner;
+        sockaddr_storage *storage;
+        // determines if this is object is responsible for deleting mSockAddr
+        bool mIsOwning;
     };
 
+    /**
+     * Wrapper around addrinfo pointer.
+     * Stores information about only one address info, and not linked list of addresses like addrinfo struct.
+     * Is responsible for deleting pointer to that address.
+     */
     class AddressInformation {
 
     public:
@@ -109,15 +151,14 @@ public:
         AddressInformation &operator=(const AddressInformation &) = delete;
 
         // move constructor
-        AddressInformation(AddressInformation &&addressInformation) noexcept: mAddrInfo(addressInformation.mAddrInfo),
+        AddressInformation(AddressInformation &&addressInformation) noexcept: mAddrInfo(
+                std::exchange(addressInformation.mAddrInfo, nullptr)),
                                                                               mAddress(std::move(
-                                                                                      addressInformation.mAddress)) {
-            addressInformation.mAddrInfo = nullptr;
-        }
+                                                                                      addressInformation.mAddress)) {}
 
         ~AddressInformation() {
             if (mAddrInfo) {
-                // #TODO why?
+                // we want to delete only our object and not entire linked list of objects.
                 mAddrInfo->ai_next = nullptr;
                 freeaddrinfo(mAddrInfo);
             }
@@ -165,11 +206,11 @@ public:
 
     // Disable copying
     Socket(const Socket &socket) = delete;
+
     Socket &operator=(const Socket &) = delete;
 
     // Enable moving
-    Socket(Socket &&socket) noexcept: mIsOpen(std::exchange(socket.mIsOpen, false)),
-                                      mFileDescriptor(std::exchange(socket.mFileDescriptor, -1)),
+    Socket(Socket &&socket) noexcept: mFileDescriptor(std::exchange(socket.mFileDescriptor, -1)),
                                       mType(socket.mType), mFamily(socket.mFamily),
                                       mProtocol(socket.mProtocol) {};
 
@@ -190,15 +231,24 @@ public:
 
     template<class T>
     requires std::ranges::sized_range<T>
-    int64_t send(T &&range, int flags = 0) const;
+    int64_t send(const T &range, int flags = 0) const;
 
-    template<typename T, std::size_t BUFFER_SIZE = DEFAULT_MAX_BUFFER_SIZE>
-    std::pair<std::array<T, BUFFER_SIZE>, int64_t> receive();
+    template<class T>
+    requires std::ranges::sized_range<T>
+    int64_t send(const Socket::Address* address, const T &range, int flags = 0) const;
+
+    template<class T>
+    std::vector<T> receive(const Socket::Address* address, int bufferSize = DEFAULT_MAX_BUFFER_SIZE, int flags = 0);
+
+    template<class T>
+    std::vector<T> receive(int bufferSize = DEFAULT_MAX_BUFFER_SIZE, int flags = 0);
 
     void close();
 
     template<typename T>
     void setOption(int option, T value);
+
+    [[nodiscard]] Socket::Address getPeerName() const;
 
     // #TODO get option
 
@@ -206,19 +256,34 @@ public:
     getAddressInfo(std::string_view address, std::string_view port, int family = 0, int type = 0, int protocol = 0,
                    int flags = 0);
 
+    class NameInfo {
+    public:
+        NameInfo(std::string mHost, std::string mService) : mHost(std::move(mHost)), mService(std::move(mService)) {}
+
+        // it this needed?
+        NameInfo(NameInfo &&nameInfo) noexcept: mHost(nameInfo.mHost), mService(nameInfo.mService) {};
+
+        const std::string mHost;
+        const std::string mService;
+    };
+
+    static NameInfo getNameInfo(const Socket::Address &address, int flags = 0);
+
     /*
      * [Section] Utility Functions
      */
 
     template<class T>
     requires std::ranges::sized_range<T>
-    void sendAll(T &&range, int flags = 0) const;
+    void sendAll(const T &range, int flags = 0) const;
 
-    template<typename T, std::size_t BUFFER_SIZE = DEFAULT_MAX_BUFFER_SIZE>
-    std::vector<T> receiveAll();
+    template<typename T>
+    std::vector<T> receiveAll(const Socket::Address* address, int bufferSize = DEFAULT_MAX_BUFFER_SIZE, int flags = 0);
 
-    template<std::size_t BUFFER_SIZE = DEFAULT_MAX_BUFFER_SIZE>
-    std::string receiveAllAsString();
+    template<typename T>
+    std::vector<T> receiveAll(int bufferSize = DEFAULT_MAX_BUFFER_SIZE, int flags = 0);
+
+    std::string receiveAllAsString(int bufferSize = DEFAULT_MAX_BUFFER_SIZE, int flags = 0);
 
     static Socket createConnection(int type, std::string_view address, int port);
 
@@ -229,7 +294,6 @@ public:
                  bool reusePort = false, bool dualStackIpv6 = false);
 
 private:
-    bool mIsOpen = true;
     int mFileDescriptor;
     int mFamily;
     int mType;
@@ -251,7 +315,7 @@ inline Socket::~Socket() {
 }
 
 inline void Socket::bind(const Socket::Address &address) const {
-    sockaddr *sockAddr = address.getPlatform();
+    sockaddr *sockAddr = address.getSockAddr();
     int status = platform_bind(mFileDescriptor, sockAddr, sockAddr->sa_len);
     if (status == -1) {
         throw Socket::Error(strerror(errno));
@@ -259,7 +323,7 @@ inline void Socket::bind(const Socket::Address &address) const {
 }
 
 inline void Socket::connect(const Socket::Address &address) const {
-    sockaddr *sockAddr = address.getPlatform();
+    sockaddr *sockAddr = address.getSockAddr();
     int status = platform_connect(mFileDescriptor, sockAddr, sockAddr->sa_len);
     if (status == -1) {
         throw Socket::Error(strerror(errno));
@@ -276,9 +340,13 @@ inline void Socket::listen(int maxBacklog) const {
 
 inline std::pair<Socket, Socket::Address> Socket::accept() const {
     Socket::Address address;
-    sockaddr *sockAddr = address.getPlatform();
-    socklen_t sockAddrSize = sizeof(*sockAddr);
-    int socketFileDescriptor = platform_accept(mFileDescriptor, sockAddr, &sockAddrSize);
+    sockaddr *sockAddr = address.getSockAddr();
+    // we get and pass size of a sockaddr_storage type instead of sockaddr
+    // as the sockaddr is stored as sockaddr_storage in Socket::Address
+    // and to support properly storing ipv6 which requires more storage
+    // we need to tell platform_accept we have space for a larger object
+    socklen_t sockStorageSize = sizeof(sockaddr_storage);
+    int socketFileDescriptor = platform_accept(mFileDescriptor, sockAddr, &sockStorageSize);
 
     if (socketFileDescriptor == -1) {
         throw Socket::Error(strerror(errno));
@@ -291,31 +359,36 @@ inline std::pair<Socket, Socket::Address> Socket::accept() const {
 
 template<class T>
 requires std::ranges::sized_range<T>
-inline int64_t Socket::send(T &&range, int flags) const {
-    int64_t bytesSend = platform_send(mFileDescriptor, std::ranges::data(range), std::ranges::size(range), flags);
-    if (bytesSend == -1) {
-        throw Socket::Error(strerror(errno));
-    }
-    return bytesSend;
+inline int64_t Socket::send(const T &range, int flags) const {
+    return send(nullptr, range, flags);
 }
 
-template<typename T, std::size_t BUFFER_SIZE>
-inline std::pair<std::array<T, BUFFER_SIZE>, int64_t> Socket::receive() {
-    // #TODO flags?
-    std::array<T, BUFFER_SIZE> buffer{};
+template<typename T>
+inline std::vector<T> Socket::receive(const Socket::Address* address, int bufferSize, int flags) {
+    std::vector<T> buffer{};
+    buffer.resize(bufferSize);
     // #FIXME BUFFER_SIZE is dependant on type
-    int64_t bytesReceived = platform_recv(mFileDescriptor, buffer.data(), BUFFER_SIZE, 0);
+    int64_t bytesReceived;
+    if (address) {
+        sockaddr_storage* storage = address->getSockStorage();
+        socklen_t storageSize = sizeof(sockaddr_storage);
+        bytesReceived = platform_recvfrom(mFileDescriptor, buffer.data(), bufferSize, flags,
+                                          reinterpret_cast<sockaddr *>(storage), &storageSize);
+    } else {
+        bytesReceived = platform_recv(mFileDescriptor, buffer.data(), bufferSize, flags);
+    }
     if (bytesReceived == -1) {
         throw Socket::Error(strerror(errno));
     }
-    return std::make_pair(buffer, bytesReceived);
+    buffer.resize(bytesReceived);
+    return buffer;
 }
 
 inline void Socket::close() {
     // #TODO check for error
-    if (mIsOpen) {
+    if (mFileDescriptor != -1) {
         platform_close(mFileDescriptor);
-        mIsOpen = false;
+        mFileDescriptor = -1;
     }
 }
 
@@ -358,7 +431,7 @@ Socket::getAddressInfo(std::string_view address, std::string_view port, int fami
 
 template<class T>
 requires std::ranges::sized_range<T>
-inline void Socket::sendAll(T &&range, int flags) const {
+inline void Socket::sendAll(const T &range, int flags) const {
     int64_t total = 0;
     int64_t left = std::ranges::size(range);
     while (total < left) {
@@ -370,23 +443,13 @@ inline void Socket::sendAll(T &&range, int flags) const {
     }
 }
 
-template<typename T, std::size_t BUFFER_SIZE>
-inline std::vector<T> Socket::receiveAll() {
-    std::vector<T> result;
-    while (true) {
-        auto [buffer, bytesReceived] = receive<T, BUFFER_SIZE>();
-        if (bytesReceived > 0) {
-            result.insert(result.end(), buffer.begin(), buffer.begin() + bytesReceived);
-        } else {
-            break;
-        }
-    }
-    return result;
+template<typename T>
+inline std::vector<T> Socket::receiveAll(int bufferSize, int flags) {
+    return receiveAll<T>(nullptr, bufferSize, flags);
 }
 
-template<std::size_t BUFFER_SIZE>
-inline std::string Socket::receiveAllAsString() {
-    auto response = receiveAll<char, BUFFER_SIZE>();
+inline std::string Socket::receiveAllAsString(int bufferSize, int flags) {
+    auto response = receiveAll<char>(bufferSize, flags);
     return {response.begin(), response.end()};
 }
 
@@ -411,7 +474,6 @@ inline Socket Socket::createConnection(int type, std::string_view address, std::
         }
     }
     throw Socket::Error("Couldn't create a connection!");
-    // free address?
 }
 
 inline Socket
@@ -440,4 +502,68 @@ Socket::createServer(std::string_view address, std::string_view port, int family
     throw Socket::Error("Failed to bind");
 }
 
+inline Socket::NameInfo Socket::getNameInfo(const Socket::Address &address, int flags) {
+    char host[1024];
+    char service[20];
+    sockaddr *addr = address.getSockAddr();
+
+    int status = platform_getnameinfo(addr, sizeof(*addr), host, sizeof(host), service, sizeof(service), flags);
+    if (status == -1) {
+        throw Socket::Error(strerror(errno));
+    }
+
+    return {host, service};
+}
+
+inline Socket::Address Socket::getPeerName() const {
+    Socket::Address address{};
+    sockaddr *addr = address.getSockAddr();
+    socklen_t addrSize = sizeof(*addr);
+    int status = platform_getpeername(mFileDescriptor, addr, &addrSize);
+    if (status == -1) {
+        throw Socket::Error(strerror(errno));
+    }
+
+    return address;
+}
+
+template<class T>
+requires std::ranges::sized_range<T>
+int64_t Socket::send(const Socket::Address* address, const T &range, int flags) const {
+
+    auto data = std::ranges::data(range);
+    uint32_t size = std::ranges::size(range);
+
+    int bytesSent;
+    if (address) {
+        const sockaddr *sockAddr = address->getSockAddr();
+        bytesSent = platform_sendto(mFileDescriptor, data, size, flags, sockAddr, sockAddr->sa_len);
+    } else {
+        bytesSent = platform_send(mFileDescriptor, data, size, flags);
+    }
+
+    if (bytesSent == -1) {
+        throw Socket::Error(strerror(errno));
+    }
+    return bytesSent;
+}
+
+template<typename T>
+std::vector<T> Socket::receiveAll(const Socket::Address *address, int bufferSize, int flags) {
+    std::vector<T> result;
+    while (true) {
+        auto buffer = receive<T>(address, bufferSize, flags);
+        if (buffer.size() > 0) {
+            result.insert(result.end(), buffer.begin(), buffer.end());
+        } else {
+            break;
+        }
+    }
+    return result;
+}
+
+template<class T>
+inline std::vector<T> Socket::receive(int bufferSize, int flags) {
+    return receive<T>(nullptr, bufferSize, flags);
+}
 #endif //BROWSER_SOCKET_H
